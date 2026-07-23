25afe2761d75b22c386e62315ae623138a84165a
visorcraft <thomas@visorcraft.com>
2026-07-23 03:19:35 -0500

    release 0.64.4

--- Tests/MongrelDBTests/DurableRetrieveTests.swift
+++ Tests/MongrelDBTests/DurableRetrieveTests.swift
@@ -270,6 +270,6 @@ final class DurableRetrieveTests: XCTestCase {
     }
 
     func testVersionIs0640() {
-        XCTAssertEqual(MongrelDBVersion.string, "0.64.3")
+        XCTAssertEqual(MongrelDBVersion.string, "0.64.4")
     }
 }
